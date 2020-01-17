51477daf4a67b7afb3bc538c395d99202c411aec
enestatli <43743872+enestatli@users.noreply.github.com>
2020-01-17 14:27:47 +0300

    init SearchBar

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { TopNav } from "./TopNav/TopNav";
 import logo from "../assets/logodentfx.png";
 import styles from "./LandingPage.module.css";
+import { SearchBar } from "../SearchBar/SearchBar";
 
 export function LandinPage() {
   return (
     <div>
       <TopNav />
       <img src={logo} alt="logo" className={styles["logo"]} />
+      <SearchBar />
     </div>
   );
 }
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./SearchBar.module.css";
+
+export function SearchBar() {
+  return (
+    <div>
+      <div className="field has-addons">
+        <p className="control">
+          <button className="button is-static is-medium">Search</button>
+        </p>
+        <p className="control">
+          <input
+            className={`input is-medium ${styles["input-control"]}`}
+            type="text"
+            placeholder="burgers, barbers, spas, hotels..."
+          />
+        </p>
+        <p className="control">
+          <button className="button is-static is-medium">@NEAR</button>
+        </p>
+        <p className="control">
+          <input
+            className={`input is-medium ${styles["input-control"]}`}
+            type="text"
+            placeholder="Where..."
+          />
+        </p>
+      </div>
+    </div>
+  );
+}
