9496c0252e01b4478f2195409b9e1bb5f7a92dcf
enestatli <43743872+enestatli@users.noreply.github.com>
2020-01-17 16:55:06 +0300

    add sub nav

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import { NavBar } from "../NavBar/NavBar";
+import { SubNav } from "../NavBar/SubNav/SubNav";
 
 export function Search() {
   return (
     <div>
       <NavBar />
+      <SubNav />
     </div>
   );
 }
