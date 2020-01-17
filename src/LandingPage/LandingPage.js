5865700ddee9deca20df3e5fa03a87ad5bcba608
enestatli <43743872+enestatli@users.noreply.github.com>
2020-01-17 13:59:12 +0300

    add logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { TopNav } from "./TopNav/TopNav";
+import logo from "../assets/logodentfx.png";
+import styles from "./LandingPage.module.css";
 
 export function LandinPage() {
   return (
     <div>
       <TopNav />
+      <img src={logo} alt="logo" className={styles["logo"]} />
     </div>
   );
 }
