153d3aff48ff0ed0083bfe8b842f730625f74e95
enestatli <43743872+enestatli@users.noreply.github.com>
2020-01-17 15:27:15 +0300

    finish landing page css

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
-import { TopNav } from "./TopNav/TopNav";
-import logo from "../assets/logodentfx.png";
+
 import styles from "./LandingPage.module.css";
+import logo from "../assets/logodentfx.png";
+
+import { TopNav } from "./TopNav/TopNav";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchSuggestion } from "./SearchSuggestion/SearchSuggestion";
 
 export function LandinPage() {
   return (
-    <div>
-      <TopNav />
-      <img src={logo} alt="logo" className={styles["logo"]} />
-      <SearchBar />
-      <SearchSuggestion />
+    <div className={styles.landing}>
+      <div className={styles["search-area"]}>
+        <TopNav />
+        <img src={logo} alt="logo" className={styles.logo} />
+        <SearchBar />
+        <SearchSuggestion />
+      </div>
     </div>
   );
 }
--- src/LandingPage/TopNav/TopNav.js
+++ src/LandingPage/TopNav/TopNav.js
@@ -4,13 +4,13 @@ import styles from "./TopNav.module.css";
 export function TopNav() {
   return (
     <div className={styles["top-nav"]}>
-      <div className={styles["left"]}>
-        <span>Write a review</span>
+      <div className={styles.left}>
+        <span>Write a Review</span>
         <span>Events</span>
       </div>
-      <div className={styles["right"]}>
-        <span>Log In</span>
-        <button className="button">Sign Up</button>
+      <div className={styles.right}>
+        <span>Login</span>
+        <button className="button">Sign up</button>
       </div>
     </div>
   );
