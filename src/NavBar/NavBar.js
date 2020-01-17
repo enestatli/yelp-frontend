e4b9ee8991dff81ad705ad015b54d7e3ae202b7d
enestatli <43743872+enestatli@users.noreply.github.com>
2020-01-17 16:03:08 +0300

    finish nav bar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -8,7 +8,7 @@ export function NavBar() {
   return (
     <div className={styles["nav-bar"]}>
       <img src={logo} alt={logo} className={styles.logo} />
-      <SearchBar />
+      <SearchBar small />
       <button className={`button ${styles["nav-button"]}`}> Sign In</button>
       <button className={`button ${styles["nav-button"]}`}> Register</button>
     </div>
